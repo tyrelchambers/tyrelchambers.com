229b255ed59bf6e5a54db94f6aaadd0af66f7d34
Tyrel Chambers <tychambers3@gmail.com>
2022-02-28 15:55:18 -0500

    index page rwd

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -31,41 +31,46 @@ export default function Index() {
       <Header />
 
       <main className="mt-14 max-w-screen-xl ml-auto mr-auto py-8">
-        <section className=" w-full grid grid-cols-2 items-center max-w-screen-xl ml-auto mr-auto gap-20">
+        <section className=" w-full flex items-center max-w-screen-xl ml-auto mr-auto gap-8 desktop:gap-20 p-4 desktop:p-0 flex-col-reverse tablet:flex-row">
           <div className="flex flex-col items-start">
-            <h1 className="max-w-2xl text-white text-5xl mt-2 leading-snug ">
+            <h1 className="max-w-2xl text-white text-3xl mt-2 leading-snug desktop:text-5xl">
               Building software and teaching people about what I love the most
             </h1>
-            <div className="flex items-center mt-10 w-full gap-6">
-              <Link to="/blog" className="link-button primary large">
+            <div className="flex flex-col items-center mt-10 w-full gap-6 desktop:flex-row">
+              <Link to="/blog" className="link-button primary large w-full">
                 Read my blog
                 <i className="fa-solid fa-arrow-right-long"></i>
               </Link>
-              <Link to="/resources" className="link-button secondary large">
+              <Link
+                to="/resources"
+                className="link-button secondary large w-full"
+              >
                 View some resources
               </Link>
             </div>
           </div>
-          <img
-            src={me2}
-            alt=""
-            className="w-full max-w-lg ml-auto mr-auto rounded-lg"
-          />
+          <div className="w-full desktop:w-1/2  max-w-lg ml-auto mr-auto">
+            <img src={me2} alt="" className="w-full rounded-lg" />
+          </div>
         </section>
         <section className="mt-20 flex justify-center items-center flex-col">
           <ScrollIcon />
         </section>
         <Gap />
-        <section>
-          <div className="flex gap-20">
-            <img src={me1} alt="" className="rounded-lg w-full max-w-xs" />
+        <section className="p-4">
+          <div className="flex gap-8 desktop:gap-20 flex-col tablet:flex-row">
+            <img
+              src={me1}
+              alt=""
+              className="rounded-lg w-full object-cover tablet:max-w-xs ml-auto mr-auto"
+            />
             <div className="flex flex-col gap-8">
               <h2 className="h2">Hey, I'm Tyrel Chambers 👋</h2>
-              <p className="text-2xl font-thin text-gray-400">
+              <p className="text-normal desktop:text-2xl font-thin text-gray-400">
                 I'm a programmer at heart and that's what you can usually find
                 me doing as a profession and a hobby.
               </p>
-              <p className="text-2xl font-thin text-gray-400">
+              <p className="text-normal desktop:text-2xl  text-gray-400">
                 Outside of programming I enjoy playing music, video games and
                 taking photographs. I also have a{" "}
                 <a href="https://youtube.com/storiesaftermidnight">
@@ -82,20 +87,20 @@ export default function Index() {
           </div>
         </section>
         <Gap />
-        <section>
+        <section className="p-4">
           <h2 className="h2">I've made some pretty cool things!</h2>
           <p className="subtitle">
             These are entirely made up of personal projects or technically
             challenges.
           </p>
-          <div className="grid grid-cols-3 gap-20 mt-16">
+          <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3  gap-8 tablet:gap-20 mt-16">
             {projects.map((project, index) => (
               <Project project={project} key={index} />
             ))}
           </div>
         </section>
         <Gap />
-        <section>
+        <section className="p-4">
           <div className="flex items-center">
             <div className="flex flex-col w-full justify-between">
               <h2 className="h2">Recently published posts</h2>
@@ -107,14 +112,14 @@ export default function Index() {
             </Link>
           </div>
 
-          <div className="grid grid-cols-3 gap-10 mt-10">
+          <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-10 mt-10">
             {posts.map((post, index) => (
               <PostItem post={post} key={index} />
             ))}
           </div>
         </section>
         <Gap />
-        <section>
+        <section className="p-4">
           <h2 className="h2">I love taking photos</h2>
           <p className="subtitle">
             If you'd like to see more, check out my{" "}
@@ -139,7 +144,7 @@ export default function Index() {
             </figcaption>
           </figure>
 
-          <div className="grid grid-cols-2 gap-10">
+          <div className="grid grid-cols-1 tablet:grid-cols-2 gap-10">
             <figure>
               <img
                 src={cousin}
@@ -164,8 +169,8 @@ export default function Index() {
           </div>
         </section>
         <Gap />
-        <section>
-          <div className="flex items-center justify-between">
+        <section className="p-4">
+          <div className="flex tablet:items-center justify-between gap-8 flex-col tablet:flex-row">
             <div className="flex flex-col max-w-3xl">
               <h2 className="h2">Resources I've collected</h2>
               <p className="subtitle">
@@ -180,7 +185,7 @@ export default function Index() {
           </div>
         </section>
         <Gap />
-        <section>
+        <section className="p-4">
           <h2 className="h2">Some media I listen to</h2>
           <div className="mt-10">
             <iframe
