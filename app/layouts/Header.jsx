710c415f16df1568ff5674c27cc006d1cb093280
Tyrel Chambers <tychambers3@gmail.com>
2022-02-28 16:16:35 -0500

    mobile nav toggle

--- app/layouts/Header.jsx
+++ app/layouts/Header.jsx
@@ -9,27 +9,33 @@ const Header = () => {
   const user = useSupabase().auth.currentUser;
 
   return (
-    <header className="max-w-screen-2xl ml-auto mr-auto flex flex-col sm:flex-row items-center justify-between w-full sm:py-10 header p-0 md:p-4">
-      <div className="flex items-center justify-between w-full p-4 md:p-0">
-        <h1 className="text-white text-2xl mb-4 sm:mb-0 mt-4 sm:mt-0">
-          Tyrel Chambers
-        </h1>
-
-        <div className="mobile-toggle w-[35px] h-[25px] flex flex-col justify-between md:hidden">
+    <header className="max-w-screen-2xl ml-auto mr-auto flex flex-col items-center justify-between w-full p-4">
+      <section className="w-full flex items-center justify-between flex-col tablet:flex-row">
+        <div className="flex w-full items-center px-4 tablet:px-0 justify-between tablet:w-fit">
+          <h1 className="text-white text-2xl mb-4 sm:mb-0 mt-4 sm:mt-0">
+            Tyrel Chambers
+          </h1>
+          <div className="mobile-toggle w-[35px] h-[25px] flex flex-col justify-between tablet:hidden">
+            <span className="h-[3px] bg-gray-400 w-full"></span>
+            <span className="h-[3px] bg-gray-400 w-full"></span>
+            <span className="h-[3px] bg-gray-400 w-full"></span>
+          </div>
+        </div>
+        <Nav />
+        <div className="flex items-center gap-4">
+          <SocialList />
+          {user?.role === "authenticated" && (
+            <Link to="/admin" className="text-gray-200 ml-6">
+              Admin
+            </Link>
+          )}
+        </div>
+        <div className="mobile-toggle w-[35px] h-[25px] flex-col justify-between hidden tablet:flex desktop:hidden">
           <span className="h-[3px] bg-gray-400 w-full"></span>
           <span className="h-[3px] bg-gray-400 w-full"></span>
           <span className="h-[3px] bg-gray-400 w-full"></span>
         </div>
-      </div>
-      <Nav />
-      <div className="flex items-center gap-4">
-        <SocialList />
-        {user?.role === "authenticated" && (
-          <Link to="/admin" className="text-gray-200 ml-6">
-            Admin
-          </Link>
-        )}
-      </div>
+      </section>
       <MobileNav />
     </header>
   );
