4c971fb7eb3fca20444c6946775c4bc00417e8d2
Tyrel Chambers <tychambers3@gmail.com>
2022-03-01 17:02:25 -0500

    add tag regex

--- app/components/BlogPosts.jsx
+++ app/components/BlogPosts.jsx
@@ -18,12 +18,13 @@ export function ErrorBoundary({ error }) {
   );
 }
 const BlogPosts = ({ posts, query }) => {
+  const tagRegex = new RegExp(decodeURIComponent(query), "i");
+
   return posts
     .filter((post) =>
       query
-        ? post.title.toLowerCase().includes(query) ||
-          post.tags.filter((tag) => tag.slug.toLowerCase().includes(query))
-            .length > 0
+        ? post.title.toLowerCase().includes(decodeURIComponent(query)) ||
+          post.tags.filter((tag) => tagRegex.test(tag.name)).length > 0
         : post
     )
     .map((post) => <PostItem key={post.title + "_" + post.id} post={post} />);
