e265bf6128e259705f76082b1b916d28156d9913
Tyrel Chambers <tychambers3@gmail.com>
2022-03-01 22:13:04 -0500

    add tech stack

--- app/routes/about.jsx
+++ app/routes/about.jsx
@@ -27,11 +27,11 @@ const about = () => {
     <div>
       <Header />
 
-      <main className="w-full max-w-screen-xl ml-auto mr-auto mt-20">
-        <section className="flex gap-8 desktop:gap-20 p-4 flex-col-reverse tablet:flex-row">
-          <div className="flex flex-col max-w-screen-sm tablet:w-1/2 w-full">
+      <main className="ml-auto mr-auto mt-20 w-full max-w-screen-xl">
+        <section className="flex flex-col-reverse gap-8 p-4 tablet:flex-row desktop:gap-20">
+          <div className="flex w-full max-w-screen-sm flex-col tablet:w-1/2">
             <h1 className="h1">Hey, I'm Tyrel Chambers 👋</h1>
-            <p className="text-xl text-blue-300 mt-4">
+            <p className="mt-4 text-xl text-blue-300">
               Software Engineer &amp; DevRel @ This Dot Labs
             </p>
             <p className="subtitle">
@@ -42,43 +42,93 @@ const about = () => {
 
             <SocialList className="mt-10" />
           </div>
-          <div className="tablet:w-1/2 w-full max-w-[450px]">
+          <div className="w-full max-w-[450px] tablet:w-1/2">
             <img
-              src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/890717F1-8B22-4767-89CF-897A7B0209EF_1_105_c.jpeg"
+              src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/890717F1-8B22-4767-89CF-897A7B0209EF_1_105_c.webp"
               alt=""
-              className="w-full object-cover rounded-lg "
+              className="w-full rounded-lg object-cover "
             />
           </div>
         </section>
 
+        <Gap height="h-20" />
+        <section className="p-4">
+          <h2 className="h2">My tech stack</h2>
+          <p className="subtitle">
+            These technologies are ones I work with on a regular basis
+          </p>
+          <ul className="mt-10 grid grid-cols-2 justify-between gap-10 tablet:grid-cols-4 desktop:grid-cols-7 ">
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+                alt=""
+                className="h-full w-full"
+              />
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <p className="text-xl text-white">Remix.run</p>
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <p className="text-xl text-white">Express.js</p>
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <img
+                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/icons/docker-icon.svg"
+                alt=""
+                className="h-full w-full"
+              />
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <img
+                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/icons/nodejs-icon.svg"
+                alt=""
+                className="h-full w-full"
+              />
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <img
+                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/icons/sequelize-icon.svg"
+                alt=""
+                className="h-full w-full"
+              />
+            </li>
+            <li className="flex h-20 items-center justify-center rounded-lg bg-zinc-800 p-4">
+              <img
+                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/icons/tailwind-css-icon.svg"
+                alt=""
+                className="h-full w-full"
+              />
+            </li>
+          </ul>
+        </section>
         <Gap />
 
         <figure className="p-4">
           <img
-            src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/821F0F1E-A38C-4939-A04C-EEB7DA2109FB_1_105_c.jpeg"
+            src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/821F0F1E-A38C-4939-A04C-EEB7DA2109FB_1_105_c.webp"
             alt=""
-            className="w-full desktop:h-[800px] object-cover rounded-lg"
+            className="w-full rounded-lg object-cover desktop:h-[800px]"
           />
-          <figcaption className="text-gray-400 text-center mt-4 max-w-xl ml-auto mr-auto">
+          <figcaption className="mt-4 ml-auto mr-auto max-w-xl text-center text-gray-400">
             My cousin on the tallest cliffs in Ontario in Thunder Bay, Ontario.
             It was a 28km round trip hike that took us all day to complete.
           </figcaption>
         </figure>
         <Gap />
         <section className="p-4">
-          <div className="flex gap-20 flex-col tablet:flex-row">
+          <div className="flex flex-col gap-20 tablet:flex-row">
             <iframe
               src="https://youtube.com/embed/uJiLFua0cww"
-              className="w-full desktop:h-[800px] object-cover rounded-lg"
+              className="w-full rounded-lg object-cover desktop:h-[800px]"
             ></iframe>
-            <div className="flex flex-col max-w-screen-sm">
+            <div className="flex max-w-screen-sm flex-col">
               <h2 className="h2 mb-4">How I got here</h2>
-              <p className="text-gray-400 leading-loose">
+              <p className="leading-loose text-gray-400">
                 I love playing music, video games and taking pictures. I'm also
                 interesting in writing blogs and teaching people about what I
                 enjoy.
               </p>
-              <p className="text-gray-400 leading-loose">
+              <p className="leading-loose text-gray-400">
                 The video here shows me and my cousin walking across the longest
                 foot suspension bridge in Canada (about 700 feet long). It was
                 incredibly windy up at that height (about 150 meters), but it
@@ -91,8 +141,8 @@ const about = () => {
         <section className="p-4">
           <div className="flex flex-col">
             <h2 className="h2">Where it began</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-20 mt-4">
-              <p className="text-gray-400 leading-loose">
+            <div className="mt-4 grid grid-cols-1 gap-20 sm:grid-cols-2">
+              <p className="leading-loose text-gray-400">
                 When I graduated from college with a Law and Security
                 Administration diploma, I wanted to go back to school for a
                 better degree. My original plan was to go to university for
@@ -102,7 +152,7 @@ const about = () => {
                 years.
               </p>
 
-              <p className="text-gray-400 leading-loose">
+              <p className="leading-loose text-gray-400">
                 My career began working as a security guard for about 9 months;
                 it was probably the worst experience in my life 😆. I then moved
                 into retail as a cashier at a sporting-goods store. After about
@@ -117,7 +167,7 @@ const about = () => {
         <Gap />
         <section className="p-4">
           <div className="flex flex-col">
-            <div className="flex tablet:items-center justify-between flex-col  tablet:flex-row">
+            <div className="flex flex-col justify-between tablet:flex-row  tablet:items-center">
               <div className="flex flex-col">
                 <h2 className="h2">I enjoy writing and teaching others</h2>
                 <p className="subtitle">
@@ -133,7 +183,7 @@ const about = () => {
               </Link>
             </div>
 
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 gap-10 mt-10">
+            <div className="mt-10 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {suggestions.map((post) => (
                 <PostItem post={post} />
               ))}
@@ -142,24 +192,24 @@ const about = () => {
         </section>
         <Gap />
         <section className="p-4">
-          <div className="flex gap-10 flex-col tablet:flex-row">
+          <div className="flex flex-col gap-10 tablet:flex-row">
             <figure className="w-full max-w-[500px]">
               <img
-                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/81EEA478-6EC4-4817-B9FA-47D0B5638095_1_105_c.jpeg"
+                src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/81EEA478-6EC4-4817-B9FA-47D0B5638095_1_105_c.webp"
                 alt=""
                 className="w-full  rounded-lg object-cover"
               />
-              <figcaption className="text-gray-400 mt-4">
+              <figcaption className="mt-4 text-gray-400">
                 Just before the void. Thunder Bay, Ontario
               </figcaption>
             </figure>
 
             <div className="flex flex-col">
               <h2 className="h2">Some fun facts about me</h2>
-              <div className="flex flex-col mt-10 gap-10">
+              <div className="mt-10 flex flex-col gap-10">
                 <div className="flex flex-col">
-                  <p className="text-white text-2xl">I can play guitar</p>
-                  <p className="text-gray-400 mt-4">
+                  <p className="text-2xl text-white">I can play guitar</p>
+                  <p className="mt-4 text-gray-400">
                     I've been playing guitar since I was a little kid. I've
                     picked up a few instruments since then such as drums, piano,
                     bass and vocals. I would love to learn the cello or violin
@@ -168,10 +218,10 @@ const about = () => {
                 </div>
 
                 <div className="flex flex-col">
-                  <p className="text-white text-2xl">
+                  <p className="text-2xl text-white">
                     I narrate thriller stories
                   </p>
-                  <p className="text-gray-400 mt-4">
+                  <p className="mt-4 text-gray-400">
                     I've been told I have a great narrating voice. So, I gave it
                     a go and upload videos occassionally to my youtube channel{" "}
                     <a
@@ -186,10 +236,10 @@ const about = () => {
                 </div>
 
                 <div className="flex flex-col">
-                  <p className="text-white text-2xl">
+                  <p className="text-2xl text-white">
                     I enjoy going for walks and runs
                   </p>
-                  <p className="text-gray-400 mt-4">
+                  <p className="mt-4 text-gray-400">
                     Enjoying the outdoors and being in nature is something I've
                     really come to enjoy. Taking pictures and taking in the
                     scenery are a favourite past-time of mine.
@@ -202,9 +252,9 @@ const about = () => {
 
         <figure className="p-4">
           <img
-            src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/1FBEB87D-AFA9-4D12-AAEE-94027686A3EA_1_105_c.jpeg"
+            src="https://kmlstyxpbbsgbimyxphk.supabase.in/storage/v1/object/public/tyrel-chambers/images/1FBEB87D-AFA9-4D12-AAEE-94027686A3EA_1_105_c.webp"
             alt=""
-            className="w-full rounded-lg desktop:h-[600px] mt-20"
+            className="mt-20 w-full rounded-lg desktop:h-[600px]"
           />
         </figure>
         <Gap />
