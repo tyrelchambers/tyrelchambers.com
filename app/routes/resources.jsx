0a9dc8db2750675b256fdff896847751f45e9477
Tyrel Chambers <tychambers3@gmail.com>
2022-03-03 11:23:19 -0500

    updaet style

--- app/routes/resources.jsx
+++ app/routes/resources.jsx
@@ -1,10 +1,12 @@
-import React from "react";
-import { Link, useLoaderData } from "remix";
-import Gap from "~/components/Gap";
 import * as resourcesList from "~/constants/resources";
-import { getImage } from "~/utils/getImage";
-import Header from "~/layouts/Header";
+
+import { Link, useLoaderData } from "remix";
+
 import Footer from "~/layouts/Footer";
+import Gap from "~/components/Gap";
+import Header from "~/layouts/Header";
+import React from "react";
+import { getImage } from "~/utils/getImage";
 
 export const loader = async () => {
   const data = resourcesList;
@@ -36,7 +38,7 @@ const resources = () => {
   return (
     <div>
       <Header />
-      <main className="max-w-screen-2xl ml-auto mr-auto mt-10 p-4">
+      <main className="ml-auto mr-auto mt-10 max-w-screen-2xl p-4">
         <div className="w-full max-w-3xl">
           <h1 className="h1">A curated list of resources</h1>
           <p className="subtitle">
@@ -44,13 +46,13 @@ const resources = () => {
             helpful stuff
           </p>
 
-          <p className="text-gray-400 mt-8">
+          <p className="mt-8 text-gray-400">
             If there are any resources that you love and would like to see in
             this list. I'd love it if you'd open an issue or submit a PR over on
             the{" "}
             <a
               href="https://github.com/tyrelchambers/tyrelchambers.com"
-              className="underline text-blue-300"
+              className="text-blue-300 underline"
               target="_blank"
               rel="noopenner noreferrer"
             >
@@ -63,16 +65,16 @@ const resources = () => {
         <div className="flex flex-col gap-20">
           <section>
             <h2 className="h2 coloured">Photography</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {photography.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -82,16 +84,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Tutorials</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {tutorials.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -101,16 +103,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Code resources</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {codeResources.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -120,16 +122,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Source control</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {sourceControl.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -139,16 +141,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Cheet sheets</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {cheatSheets.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -158,16 +160,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Colour pickers</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {colourPickers.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -177,16 +179,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Articles</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {articles.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -196,16 +198,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">VPS</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {servers.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -215,16 +217,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Communication</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {communication.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -234,16 +236,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Design software</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {designSoftware.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -253,16 +255,16 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Project management</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {projectManagement.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all"
+                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
                 >
                   <h3 className="h3">{item.title}</h3>
-                  <p className="text-gray-400 text-xl mt-4">
+                  <p className="mt-4 text-xl text-gray-400">
                     {item.description}
                   </p>
                 </a>
@@ -272,24 +274,24 @@ const resources = () => {
 
           <section>
             <h2 className="h2 coloured">Books</h2>
-            <div className="grid grid-cols-1 tablet:grid-cols-2 desktop:grid-cols-3 mt-8 gap-10">
+            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
               {books.map((item, index) => (
                 <a
                   href={item.url}
                   key={index}
                   target="_blank"
-                  className="border-2 border-gray-700 rounded-lg p-6 hover:border-yellow-300 transition-all flex flex-col mobile:flex-row gap-6 items-center"
+                  className="flex flex-col items-center gap-6 rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300 mobile:flex-row tablet:items-start"
                 >
                   <img
                     src={item.imageURL}
                     alt=""
-                    className="w-32 object-cover  rounded-lg aspect-[4/6]"
+                    className="aspect-[4/6] w-32  rounded-lg object-cover"
                   />
                   <div className="flex flex-col">
                     <h3 className="h3 text-center mobile:text-left">
                       {item.title}
                     </h3>
-                    <p className="text-gray-400 text-xl mt-4 text-center mobile:text-left">
+                    <p className="mt-4 text-center text-xl text-gray-400 mobile:text-left">
                       {item.author}
                     </p>
                   </div>
