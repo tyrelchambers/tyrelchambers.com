a7d481afca5c791dee556b2ef96d4b99636978e9
Tyrel Chambers <tychambers3@gmail.com>
2022-03-08 15:48:22 -0500

    prefetch on nav links

--- app/layouts/Nav.jsx
+++ app/layouts/Nav.jsx
@@ -1,27 +1,27 @@
-import React from "react";
 import { NavLink } from "remix";
+import React from "react";
 
 const Nav = () => {
   return (
     <nav className="nav hidden desktop:flex">
       <ul className="flex items-center gap-6">
         <li>
-          <NavLink to="/" className="nav-link">
+          <NavLink to="/" className="nav-link" prefetch="intent">
             Home
           </NavLink>
         </li>
         <li>
-          <NavLink to="/blog" className="nav-link">
+          <NavLink to="/blog" className="nav-link" prefetch="intent">
             Blog
           </NavLink>
         </li>
         <li>
-          <NavLink to="/resources" className="nav-link">
+          <NavLink to="/resources" className="nav-link" prefetch="intent">
             Resources
           </NavLink>
         </li>
         <li>
-          <NavLink to="/about" className="nav-link">
+          <NavLink to="/about" className="nav-link" prefetch="intent">
             About
           </NavLink>
         </li>
