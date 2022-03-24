a3b60dc1cef9e62a12223deb5a7bd6e2ab8c36aa
Tyrel Chambers <tychambers3@gmail.com>
2022-03-24 11:19:33 -0400

    update packages

--- app/routes/signout.jsx
+++ app/routes/signout.jsx
@@ -1,6 +1,7 @@
+import { destroySession, getSession } from "../supabase.server";
+
 import React from "react";
 import { redirect } from "remix";
-import { destroySession, getSession } from "../supabase.server";
 
 export const loader = () => {
   // Redirect to `/` if user tried to access `/signout`
