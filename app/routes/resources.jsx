27d6863036bac2fd8218e76cc46ab7126c86d47c
Tyrel Chambers <tychambers3@gmail.com>
2022-04-10 13:48:11 -0400

    refactor resources page

--- app/routes/resources.jsx
+++ app/routes/resources.jsx
@@ -1,6 +1,4 @@
-import * as resourcesList from "~/constants/resources";
-
-import { Link, useLoaderData } from "remix";
+import { Link, useLoaderData, useSearchParams } from "remix";
 
 import Footer from "~/layouts/Footer";
 import Gap from "~/components/Gap";
@@ -8,33 +6,49 @@ import Header from "~/layouts/Header";
 import React from "react";
 import SectionHero from "~/layouts/SectionHero";
 import { getImage } from "~/utils/getImage";
+import { resources as resourceList } from "~/constants/resources";
+import { useUpdateQueryStringValueWithoutNavigation } from "~/utils/misc";
 
 export const loader = async () => {
-  const data = resourcesList;
+  const data = resourceList;
 
-  for (let index = 0; index < data.books.length; index++) {
-    const element = data.books[index];
-    element.imageURL = await getImage(element.img);
+  for (let index = 0; index < data.length; index++) {
+    const element = data[index];
+
+    for (let j = 0; j < element.children.length; j++) {
+      const child = element.children[j];
+      if (child.img) {
+        child.imageURL = await getImage(child.img);
+      }
+
+      element.children[j] = child;
+    }
   }
 
   return data;
 };
 
 const resources = () => {
-  const {
-    photography,
-    tutorials,
-    codeResources,
-    sourceControl,
-    cheatSheets,
-    colourPickers,
-    articles,
-    servers,
-    communication,
-    designSoftware,
-    projectManagement,
-    books,
-  } = useLoaderData();
+  const data = useLoaderData();
+  let [searchParams] = useSearchParams();
+
+  const filteredList = data
+    .sort((a, b) => {
+      if (a.title < b.title) {
+        return -1;
+      }
+      if (a.title > b.title) {
+        return 1;
+      }
+      return 0;
+    })
+    .map((item) => item.title);
+
+  const [queryValue, setQuery] = React.useState(() => {
+    return searchParams.get("q") ?? "";
+  });
+
+  useUpdateQueryStringValueWithoutNavigation("q", queryValue);
 
   return (
     <div>
@@ -67,244 +81,65 @@ const resources = () => {
           }
         />
 
-        <Gap />
+        <div className="mt-16 mb-16 flex flex-wrap items-center gap-6">
+          {filteredList.map((item) => (
+            <button
+              className={`whitespace-nowrap rounded-full  py-2 px-4  transition-all  hover:text-white ${
+                queryValue === item
+                  ? "bg-yellow-300 text-gray-800 hover:bg-red-300"
+                  : "bg-gray-800 text-gray-300 hover:bg-gray-700"
+              }`}
+              onClick={() =>
+                queryValue === item ? setQuery("") : setQuery(item)
+              }
+              key={item}
+            >
+              {item}
+            </button>
+          ))}
+        </div>
+
         <div className="flex flex-col gap-20">
-          <section>
-            <h2 className="h2 coloured">Photography</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {photography.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Tutorials</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {tutorials.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Code resources</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {codeResources.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Source control</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {sourceControl.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Cheet sheets</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {cheatSheets.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Colour pickers</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {colourPickers.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Articles</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {articles.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">VPS</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {servers.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Communication</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {communication.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Design software</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {designSoftware.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Project management</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {projectManagement.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300"
-                >
-                  <h3 className="h3">{item.title}</h3>
-                  <p className="mt-4 text-xl text-gray-400">
-                    {item.description}
-                  </p>
-                </a>
-              ))}
-            </div>
-          </section>
-
-          <section>
-            <h2 className="h2 coloured">Books</h2>
-            <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
-              {books.map((item, index) => (
-                <a
-                  href={item.url}
-                  key={index}
-                  target="_blank"
-                  className="flex flex-col items-center gap-6 rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300 mobile:flex-row tablet:items-start"
-                >
-                  <img
-                    src={item.imageURL}
-                    alt=""
-                    className="aspect-[4/6] w-32  rounded-lg object-cover"
-                  />
-                  <div className="flex flex-col">
-                    <h3 className="h3 text-center mobile:text-left">
-                      {item.title}
-                    </h3>
-                    <p className="mt-4 text-center text-xl text-gray-400 mobile:text-left">
-                      {item.author}
-                    </p>
-                  </div>
-                </a>
-              ))}
-            </div>
-          </section>
+          {data
+            .filter((item) => (queryValue ? item.title === queryValue : item))
+            .map((resource) => (
+              <section key={resource.title}>
+                <h2 className="h2 coloured">{resource.title}</h2>
+                <div className="mt-8 grid grid-cols-1 gap-10 tablet:grid-cols-2 desktop:grid-cols-3">
+                  {resource.children.map((item, index) => (
+                    <a
+                      href={item.url}
+                      key={index}
+                      target="_blank"
+                      className="flex flex-col items-center gap-6 rounded-lg border-2 border-gray-700 p-6 transition-all hover:border-yellow-300 mobile:flex-row tablet:items-start"
+                    >
+                      {item.imageURL && (
+                        <img
+                          src={item.imageURL}
+                          alt=""
+                          className="aspect-[4/6] w-32  rounded-lg object-cover"
+                        />
+                      )}
+                      <div className="flex flex-col">
+                        <h3 className="h3 text-center mobile:text-left">
+                          {item.title}
+                        </h3>
+                        {item.author && (
+                          <p className="mt-4 text-center text-xl text-gray-400 mobile:text-left">
+                            {item.author}
+                          </p>
+                        )}
+                        {item.description && (
+                          <p className="mt-4 text-center text-xl text-gray-400 mobile:text-left">
+                            {item.description}
+                          </p>
+                        )}
+                      </div>
+                    </a>
+                  ))}
+                </div>
+              </section>
+            ))}
         </div>
       </main>
       <Footer />
