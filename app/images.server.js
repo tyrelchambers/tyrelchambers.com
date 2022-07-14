dd2e497e6f41e7687121c852f34579dad758da1d
Tyrel Chambers <tychambers3@gmail.com>
2022-07-14 18:43:16 -0400

    mybe?

--- app/images.server.js
+++ app/images.server.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 export function getImage(filename) {
-  const images = fs.readdirSync("./public/images");
+  const images = fs.readdirSync("~/public/images");
   const image = images.find((image) => image.includes(filename));
 
-  return fs.readFileSync(`./public/images/${image}`, "base64");
+  return fs.readFileSync(`~/public/images/${image}`, "base64");
 }
