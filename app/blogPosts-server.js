7844157cfb1de306788b6b6b8e0801730573af01
Tyrel Chambers <tychambers3@gmail.com>
2022-07-15 20:37:48 -0400

    stuff that wasnt big

--- app/blogPosts-server.js
+++ app/blogPosts-server.js
@@ -1,12 +1,12 @@
-import { bundleMdx } from "./compile-mdx.server";
-import { devToPublishedArticles } from "~/api/devToPublishedArticles";
-import { getHashnodeStats } from "./api/getHashnodeStats";
+import React from "react";
 import { supabase } from "~/supabase.server";
 
 export async function getPost(slug) {
   const { default: rehypeStringify } = await import("rehype-stringify");
   const { default: remarkParse } = await import("remark-parse");
   const { default: remarkRehype } = await import("remark-rehype");
+  const { default: rehypeReact } = await import("rehype-react");
+
   const { unified } = await import("unified");
 
   const { body: post, error } = await supabase
@@ -18,10 +18,17 @@ export async function getPost(slug) {
   const markdown = await unified()
     .use(remarkParse)
     .use(remarkRehype)
+    .use(rehypeReact, {
+      createElement: React.createElement,
+    })
     .use(rehypeStringify)
     .process(post.markdown);
 
-  return { post, error, markdown: markdown.value };
+  return {
+    post,
+    error,
+    markdown: markdown.value,
+  };
 }
 
 export async function getPostById(id) {
@@ -54,45 +61,6 @@ export async function getPosts() {
       ascending: false,
     });
   return { posts, error };
-}
-
-export async function getDevToStats() {
-  const articles = await devToPublishedArticles();
-  const stats = articles.data
-    .map((art) => ({
-      comments_count: art.comments_count,
-      public_reactions_count: art.public_reactions_count,
-      positive_reactions_count: art.positive_reactions_count,
-      page_views_count: art.page_views_count,
-    }))
-    .reduce((acc, curr) => {
-      acc.comments_count += curr.comments_count;
-      acc.public_reactions_count += curr.public_reactions_count;
-      acc.positive_reactions_count += curr.positive_reactions_count;
-      acc.page_views_count += curr.page_views_count;
-      return acc;
-    });
-
-  return stats;
-}
-
-export async function hashNodeStats() {
-  const posts = await getHashnodeStats();
-
-  const stats = posts
-    .map((post) => ({
-      totalReactions: post.totalReactions,
-      replyCount: post.replyCount,
-      responseCount: post.responseCount,
-    }))
-    .reduce((acc, curr) => {
-      acc.totalReactions += curr.totalReactions;
-      acc.replyCount += curr.replyCount;
-      acc.responseCount += curr.responseCount;
-      return acc;
-    });
-
-  return stats;
 }
 
 export async function triggerView(id) {
